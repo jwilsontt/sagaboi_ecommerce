dee6dfee07cdaabd51b069e8aa38e37ddb4fa8b1
jwilsontt <jwilsontt@gmail.com>
2018-08-08 23:33:41 -0400

    Finalize checkout process

--- src/orders/models.py
+++ src/orders/models.py
@@ -2,6 +2,7 @@ import math
 from django.db import models
 from django.db.models.signals import pre_save,post_save
 
+from addresses.models import Address
 from billing.models import BillingProfile
 from carts.models import Cart
 from sagaboishop.utils import unique_order_id_generator
@@ -20,7 +21,9 @@ class OrderManager(models.Manager):
         qs = self.get_queryset().filter(
                 billing_profile=billing_profile,
                 cart=cart_obj,
-                active=True)
+                active=True,
+                status='created'
+                )
         if qs.count() == 1:
             obj = qs.first()
         else:
@@ -31,9 +34,8 @@ class OrderManager(models.Manager):
 class Order(models.Model):
     billing_profile     = models.ForeignKey(BillingProfile, null=True, blank=True)
     order_id            = models.CharField(max_length=120, blank=True)
-    #billing_profile    =
-    #shipping_address   =
-    #billing_address    =
+    shipping_address    = models.ForeignKey(Address, related_name="shipping_address", null=True, blank=True)
+    billing_address     = models.ForeignKey(Address, related_name="billing_address", null=True, blank=True)
     cart                = models.ForeignKey(Cart)
     status              = models.CharField(max_length=120, default='created', choices=ORDER_STATUS_CHOICES)
     shipping_total      = models.DecimalField(default=5.99, max_digits=10, decimal_places=2)
@@ -57,6 +59,21 @@ class Order(models.Model):
         # print(type(self.total))
         self.save()
         return formatted_total
+
+    def check_done(self):
+        billing_profile = self.billing_address
+        shipping_address = self.shipping_address
+        billing_address = self.billing_address
+        total = self.total
+        if billing_profile and shipping_address and billing_address and total > 0:
+            return True
+        return False
+
+    def mark_paid(self):
+        if self.check_done():
+            self.status = "paid"
+            self.save()
+        return self.status
 
 
 def pre_save_create_order_id(sender, instance, *args, **kwargs):
